b72c768eab7bbf17bc75051f1edf0affb411cc3d
Ninglo <jojiu7609@outlook.com>
2020-10-11 19:14:12 +0800

    test CORS

--- app.js
+++ app.js
@@ -29,6 +29,21 @@ app.use('/apii/', (req, res) => {
 
 // route area
 app.use('/', indexRouter);
+app.get('/test', (req, res) => {
+    console.log(req.cookies)
+    res.cookie('111', '2333')
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization,'Origin',Accept,X-Requested-With");
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header("Content-Type", "application/json;charset=utf-8");
+    console.log('get')
+    res.send('1')
+})
+app.options('/test', (req, res) => {
+    console.log(req.cookies)
+    console.log('options')
+    res.send('1')
+})
 app.use('/users', usersRouter);
 app.use('/doctors', doctorsRouter)
 app.use('/api', apiRouter)
